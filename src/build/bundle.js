28649f833757cc98a90327679ed071d803b8a703
kylin <396882574@qq.com>
2017-01-19 17:53:53 +0800

    按键映射

--- src/build/bundle.js
+++ src/build/bundle.js
@@ -102,6 +102,10 @@ document.addEventListener('keydown', function (event) {
 				for (i = 0; i < index; i++) {
 						frequencyRatio *= frequencyRatioTempered;
 				}
+				// style
+				var elm = keyboards[59 - (n * 12 + index)];
+				addClass(elm, 'on');
+
 				bufferSource = ctxs[n].createBufferSource();
 				bufferSource.buffer = datas[n];
 
@@ -114,9 +118,34 @@ document.addEventListener('keyup', function (event) {
 
 		var index = keymap.indexOf(event.keyCode);
 
+		var n = 2;
 		if (~index) {
 				keyIsDown[index] = false;
 		}
+		var elm = keyboards[59 - (n * 12 + index)];
+		removeClass(elm, 'on');
 });
+
+function hasClass(elem, cls) {
+		cls = cls || '';
+		if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
+		return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
+}
+
+function addClass(ele, cls) {
+		if (!hasClass(ele, cls)) {
+				ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
+		}
+}
+
+function removeClass(ele, cls) {
+		if (hasClass(ele, cls)) {
+				var newClass = ' ' + ele.className.replace(/[\t\r\n]/g, '') + ' ';
+				while (newClass.indexOf(' ' + cls + ' ') >= 0) {
+						newClass = newClass.replace(' ' + cls + ' ', ' ');
+				}
+				ele.className = newClass.replace(/^\s+|\s+$/g, '');
+		}
+}
 
 }());
